fb2ff74cb70be1672c8b7f8abcd7091d0ee2dfc9
AKShaw <ashaw1297@gmail.com>
2021-02-16 16:14:07 +0000

    Can now add new/update commands through Discord.

--- ClazBot.py
+++ ClazBot.py
@@ -1,5 +1,6 @@
 import os
 import json
+import shlex
 
 import discord
 from dotenv import load_dotenv
@@ -9,14 +10,58 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+#Build a string of commands from the messages dictionary
+def get_commands_string():
+    string = "Commands: "
+    for key in messages.keys():
+        string += "\n\t" + key
+        
+    string += "\n\t!clazbot add <command> <reply> <image> \t <command> doesn't need a !. <reply> must be in quotes. <image> is optional but must be a URL if provided."
+    string += "\n\t!clazbot update <command> <reply> <image> \t Same as !clazbot add."
+    return string
+
+
+#Add a message to the ditionary, return false if it already exists
+def add_message(command, reply, image):
+    if command in messages:
+        return False
+    else:
+        return update_message(command, reply, image)
+
+
+#Update a message in the dictionary.
+def update_message(command, reply, image):
+    messages[command] = [reply, image]
+    print(command, " changed to ", reply, " ", str(image))
+    save_messages()
+    return True
+
+def save_messages():
+    with open("messages.json", "w") as outfile:  
+        json.dump(messages, outfile) 
+    
+#Get the command, reply and image from a message and call the appropriate method to add or update it.
+def parse_add_update(message_parts, update):
+    command = "!"+message_parts[2]
+    reply = message_parts[3]
+    image = None
+    
+    if len(message_parts)==5:
+        image = message_parts[4]
+    
+    if update:
+        print("Updating ", command, " in dictionary.")
+        return update_message(command, reply, image)
+    else:
+        print("Adding ", command, " to dictionary.")
+        return add_message(command, reply, image)
+    
 @client.event
 async def on_ready():
+    #Load the messages.json file into a dictionary with the command as the key
     with open("messages.json") as json_file:
         global messages
-        messages = {}
-        json_data = json.load(json_file)
-        for comm in json_data["commands"]:
-            messages[str(comm["evoker"])] = [str(comm["response"]["message"]), str(comm["response"]["image"])]
+        messages = json.load(json_file)
     
     print(f'{client.user} has connected to Discord!')
     
@@ -26,26 +71,47 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    message_parts = message.content.split(" ")
+    message_parts = shlex.split(message.content)
     start = message_parts[0].lower()
+    second_part = ""
+    if len(message_parts)!=1:
+        second_part = message_parts[1].lower()
 
     if start=="!clazbot":
         #Add, update messages in here, help etc
-        if message_parts[1].lower()=="help":
-            await message.channel.send("Commands: " + str(messages.keys()))
+        if second_part=="help":
+            await message.channel.send(get_commands_string())
+        elif second_part=="add":
+            command = "!"+message_parts[2]
+        
+            #Get the command, reply and image (if applicable) and call the add_message method
+            result = parse_add_update(message_parts, False)
+            
+            if result==False:
+                await message.channel.send("Command '" + command + "' is already in use, please use !clazbot update instead.")
+            else:
+                await message.channel.send("Added '" + command + "' successfully.")
+        elif second_part=="update":
+            command = "!"+message_parts[2]
+            result = parse_add_update(message_parts, True)
+            
+            await message.channel.send("Updated '" + command + "'.")
+        elif second_part=="dump":
+            save_messages()
+            await message.channel.send("Dumped to sample.json")
         else:
             await message.channel.send('Heh-low lez')
     
+    #If the command isnt a clazbot command, look for it in the messages dictionary.
     if start in messages:
         text = messages[start][0]
         img_path = messages[start][1]
         
-        print("Message: ", text)
-        print("image: ", img_path)
-        embed = discord.Embed()
-        embed.set_image(url=img_path)
-        await message.channel.send(text, embed=embed)
-       
-    print(messages.keys())
+        if img_path is not None:
+            embed = discord.Embed()
+            embed.set_image(url=img_path)
+            await message.channel.send(text, embed=embed)
+        else:
+            await message.channel.send(text)
 
 client.run(TOKEN)
